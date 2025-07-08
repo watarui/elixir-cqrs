838341088de6bdbe561652fc81108c8b02e094c7
Wataru Ishikawa <wataruby@gmail.com>
2025-07-08 12:47:47 +0900

    リファクタリング: factory.exの読み込みをコメントアウトしました

--- apps/client_service/test/test_helper.exs
+++ apps/client_service/test/test_helper.exs
@@ -1,5 +1,5 @@
 # Test helper設定をロード
-Code.require_file("../../../test/support/factory.ex", __DIR__)
+# Code.require_file("../../../test/support/factory.ex", __DIR__)
 Code.require_file("../../../test/support/test_helpers.ex", __DIR__)
 Code.require_file("../../../test/support/graphql_helpers.ex", __DIR__)
 
--- apps/query_service/test/test_helper.exs
+++ apps/query_service/test/test_helper.exs
@@ -1,5 +1,5 @@
 # Test helper設定をロード
-Code.require_file("../../../test/support/factory.ex", __DIR__)
+# Code.require_file("../../../test/support/factory.ex", __DIR__)
 Code.require_file("../../../test/support/test_helpers.ex", __DIR__)
 Code.require_file("../../../test/support/event_store_helpers.ex", __DIR__)
 
